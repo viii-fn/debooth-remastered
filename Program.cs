a35488bfc93e60ea01f673c98113483e881aa377
viii-fn <irhayeelvisebah@gmail.com>
2026-09-15 03:26:55 +0100

    Christ

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using debooth.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AppDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
