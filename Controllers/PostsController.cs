131c91e34473a47d074c11f2fb164dbdb9cc4a1e
viii-fn <irhayeelvisebah@gmail.com>
2026-09-15 03:15:46 +0100

    ###

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -15,13 +15,13 @@ public class UserPostsController : ControllerBase
 	}
 
 	[HttpGet]
-	public async Task<ActionResult<IEnumerable<UserPost>>> GetPosts()
+	public async Task<ActionResult<IEnumerable<UserPost>>> GetUserPosts()
 	{
-		return await _context.Posts.ToListAsync();
+		return await _context.UserPosts.ToListAsync();
 	}
 
 	[HttpPost]
-	public async Task<ActionResult<UserPost>> UserPostsPost(UserPost post)
+	public async Task<ActionResult<UserPost>> PostUserPosts(UserPost post)
 	{
 		_context.UserPosts.Add(post);
 		await _context.SaveChangesAsync();
