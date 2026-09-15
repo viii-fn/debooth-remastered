cb3882c03e00495f677eaf52326e3aa65bf7849d
viii-fn <irhayeelvisebah@gmail.com>
2026-09-15 21:12:21 +0100

    Major change: implemented get by id endpoint

--- Controllers/UserPostsController.cs
+++ Controllers/UserPostsController.cs
@@ -7,7 +7,7 @@ using debooth.Models;
 [ApiController]
 public class UserPostsController : ControllerBase
 {
-	private readonly AppDbContext _context;
+	private AppDbContext _context;
 
 	public UserPostsController(AppDbContext context)
 	{
@@ -15,16 +15,28 @@ public class UserPostsController : ControllerBase
 	}
 
 	[HttpGet]
-	public async Task<ActionResult<IEnumerable<UserPost>>> GetUserPosts()
+	public async Task<ActionResult<IEnumerable<UserPost>>> GetAll()
 	{
 		return await _context.UserPosts.ToListAsync();
 	}
 
+	[HttpGet("{id}")]
+	public async Task<ActionResult<UserPost>> GetById(int id)
+	{
+		var post = await _context.UserPosts.FindAsync(id);
+		if (post == null)
+		{
+			return NotFound();
+		}
+
+		return post;
+	}
+
 	[HttpPost]
-	public async Task<ActionResult<UserPost>> PostUserPosts(UserPost post)
+	public async Task<ActionResult<UserPost>> Create(UserPost post)
 	{
 		_context.UserPosts.Add(post);
 		await _context.SaveChangesAsync();
-		return CreatedAtAction(nameof(GetUserPosts), new { id = post.Id }, post);
+		return CreatedAtAction(nameof(GetById), new { id = post.Id }, post);
 	}
 }
