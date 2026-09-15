26d120c76cbf11341e5d989198a8a49011510445
viii-fn <irhayeelvisebah@gmail.com>
2026-09-15 01:49:42 +0100

    #

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -3,6 +3,7 @@ namespace debooth.Models;
 public class PostModel
 {
 	public int Id { get; set; }
+	[Required]
 	public string Content { get; set; } = string.Empty;
 	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
