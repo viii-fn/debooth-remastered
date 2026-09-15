ac5cabb67ccc8b752d5a40fcaec702da20e70c65
viii-fn <irhayeelvisebah@gmail.com>
2026-09-15 01:30:50 +0100

    Configured get request for posts

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -7,4 +7,15 @@ using debooth.Models;
 public class PostsController : ControllerBase
 {
 	private readonly AppDbContext _context;
+
+	public PostsController(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Post<ActionResult<IEnumerable<Post>>> GetPosts()
+	{
+		return await _context.Posts.ToListAsync();
+	}
 }
